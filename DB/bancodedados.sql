b1bc52db7eae3a4f047be524d3cb8b6d53ba1791
João Victor de Aquino Netto Paciello <joaovictorpaciello@gmail.com>
2024-09-13 23:45:08 -0300

    Banco de dados atualizado

--- DB/bancodedados.sql
+++ DB/bancodedados.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 14/09/2024 às 04:06
+-- Tempo de geração: 14/09/2024 às 04:44
 -- Versão do servidor: 10.4.28-MariaDB
 -- Versão do PHP: 8.0.28
 
@@ -39,7 +39,8 @@ CREATE TABLE `clicks_diarios` (
 
 INSERT INTO `clicks_diarios` (`id`, `usuario_id`, `data`) VALUES
 (1, 1, '2024-09-13'),
-(2, 3, '2024-09-13');
+(2, 3, '2024-09-13'),
+(3, 4, '2024-09-13');
 
 -- --------------------------------------------------------
 
@@ -67,17 +68,20 @@ CREATE TABLE `usuarios` (
   `senha` varchar(255) NOT NULL,
   `nome` varchar(100) DEFAULT NULL,
   `imagem_perfil` varchar(255) DEFAULT NULL,
-  `data_cadastro` timestamp NOT NULL DEFAULT current_timestamp()
+  `data_cadastro` timestamp NOT NULL DEFAULT current_timestamp(),
+  `idade` int(11) DEFAULT NULL,
+  `sexo` enum('Masculino','Feminino','Outro') DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Despejando dados para a tabela `usuarios`
 --
 
-INSERT INTO `usuarios` (`id`, `usuario`, `email`, `senha`, `nome`, `imagem_perfil`, `data_cadastro`) VALUES
-(1, 'joao', 'teste@gmail.com', '$2y$10$FMWWebT86DVxvIM6y9v8auZbccedkRrBPBQge8YVTZnVYyfG0Zu62', 'joao', '../Assets/profile_images/66e4dfdebee26.png', '2024-09-11 16:23:24'),
-(2, 'joao', 'nao@gmail.com', '$2y$10$BXzo37Mkr1vuqYB2sLVCZ.q79mbCEkSD/ssv4x2mefyf0sf8D7/0q', NULL, NULL, '2024-09-11 17:45:22'),
-(3, 'joao123', 'para@gmail.com', '$2y$10$16FvCfw0Z1EuUYxafTriO.n51A/pUll8FHAnUQ3ImebEZ.NfMX7Gu', 'Geovani', '../Assets/profile_images/66e4edda76d51.png', '2024-09-11 19:10:08');
+INSERT INTO `usuarios` (`id`, `usuario`, `email`, `senha`, `nome`, `imagem_perfil`, `data_cadastro`, `idade`, `sexo`) VALUES
+(1, 'joao', 'teste@gmail.com', '$2y$10$FMWWebT86DVxvIM6y9v8auZbccedkRrBPBQge8YVTZnVYyfG0Zu62', 'joao', '../Assets/profile_images/66e4dfdebee26.png', '2024-09-11 16:23:24', NULL, NULL),
+(2, 'joao', 'nao@gmail.com', '$2y$10$BXzo37Mkr1vuqYB2sLVCZ.q79mbCEkSD/ssv4x2mefyf0sf8D7/0q', NULL, NULL, '2024-09-11 17:45:22', NULL, NULL),
+(3, 'joao123', 'para@gmail.com', '$2y$10$16FvCfw0Z1EuUYxafTriO.n51A/pUll8FHAnUQ3ImebEZ.NfMX7Gu', 'Geovani', '../Assets/profile_images/66e4edda76d51.png', '2024-09-11 19:10:08', 20, 'Masculino'),
+(4, 'breno', 'breno@gmail.com', '$2y$10$0V9wAUuiW8DucIHg4NtKQ.mXr8Tgo2c1aKVPPjChn0uQcltNRNXay', 'Breno', '../Assets/profile_images/66e4f808e261c.png', '2024-09-14 02:41:15', 42, 'Masculino');
 
 --
 -- Índices para tabelas despejadas
@@ -111,7 +115,7 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de tabela `clicks_diarios`
 --
 ALTER TABLE `clicks_diarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de tabela `tasks`
@@ -123,7 +127,7 @@ ALTER TABLE `tasks`
 -- AUTO_INCREMENT de tabela `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Restrições para tabelas despejadas
