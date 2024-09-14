8f80997112c89d688c5d63791260a10b6aadd277
João Victor de Aquino Netto Paciello <joaovictorpaciello@gmail.com>
2024-09-13 23:07:40 -0300

    atualização no banco de dados

--- DB/bancodedados.sql
+++ DB/bancodedados.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3307
--- Tempo de geração: 12/09/2024 às 22:34
--- Versão do servidor: 10.4.32-MariaDB
--- Versão do PHP: 8.2.12
+-- Host: 127.0.0.1
+-- Tempo de geração: 14/09/2024 às 04:06
+-- Versão do servidor: 10.4.28-MariaDB
+-- Versão do PHP: 8.0.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -20,6 +20,26 @@ SET time_zone = "+00:00";
 --
 -- Banco de dados: `taskify`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `clicks_diarios`
+--
+
+CREATE TABLE `clicks_diarios` (
+  `id` int(11) NOT NULL,
+  `usuario_id` int(11) NOT NULL,
+  `data` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `clicks_diarios`
+--
+
+INSERT INTO `clicks_diarios` (`id`, `usuario_id`, `data`) VALUES
+(1, 1, '2024-09-13'),
+(2, 3, '2024-09-13');
 
 -- --------------------------------------------------------
 
@@ -55,13 +75,20 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`id`, `usuario`, `email`, `senha`, `nome`, `imagem_perfil`, `data_cadastro`) VALUES
-(1, 'joao', 'teste@gmail.com', '$2y$10$FMWWebT86DVxvIM6y9v8auZbccedkRrBPBQge8YVTZnVYyfG0Zu62', 'JOOAO', 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAABVYAAAMACAYAAADPPjzCAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAP+6SURBVHhe7N0LYE/l/wfw9+4X2zDXuUZGjLJui1AuJaSkoiSXlFJKCV1I/0QlUqSEREJR+enikkhRsoq5jTSpuc112P373e3/+TznfGebbTZR2Pul0znf59y/O8855/l8n/Mct+Tk5GwQERERE', '2024-09-11 16:23:24'),
+(1, 'joao', 'teste@gmail.com', '$2y$10$FMWWebT86DVxvIM6y9v8auZbccedkRrBPBQge8YVTZnVYyfG0Zu62', 'joao', '../Assets/profile_images/66e4dfdebee26.png', '2024-09-11 16:23:24'),
 (2, 'joao', 'nao@gmail.com', '$2y$10$BXzo37Mkr1vuqYB2sLVCZ.q79mbCEkSD/ssv4x2mefyf0sf8D7/0q', NULL, NULL, '2024-09-11 17:45:22'),
-(3, 'joao123', 'para@gmail.com', '$2y$10$16FvCfw0Z1EuUYxafTriO.n51A/pUll8FHAnUQ3ImebEZ.NfMX7Gu', NULL, NULL, '2024-09-11 19:10:08');
+(3, 'joao123', 'para@gmail.com', '$2y$10$16FvCfw0Z1EuUYxafTriO.n51A/pUll8FHAnUQ3ImebEZ.NfMX7Gu', 'Geovani', '../Assets/profile_images/66e4edda76d51.png', '2024-09-11 19:10:08');
 
 --
 -- Índices para tabelas despejadas
 --
+
+--
+-- Índices de tabela `clicks_diarios`
+--
+ALTER TABLE `clicks_diarios`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `usuario_id_data` (`usuario_id`,`data`);
 
 --
 -- Índices de tabela `tasks`
@@ -81,6 +108,12 @@ ALTER TABLE `usuarios`
 --
 
 --
+-- AUTO_INCREMENT de tabela `clicks_diarios`
+--
+ALTER TABLE `clicks_diarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT de tabela `tasks`
 --
 ALTER TABLE `tasks`
@@ -91,6 +124,16 @@ ALTER TABLE `tasks`
 --
 ALTER TABLE `usuarios`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Restrições para tabelas despejadas
+--
+
+--
+-- Restrições para tabelas `clicks_diarios`
+--
+ALTER TABLE `clicks_diarios`
+  ADD CONSTRAINT `clicks_diarios_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
